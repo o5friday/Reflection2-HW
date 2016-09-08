d58258b98202a5063e820ad4bdf9a9f6c4678265
o5friday <darya.yemzhina@gmail.com>
2016-09-08 03:23:58 +0300

    init

---
+++ ReflectionConsoleApplication/ReflectionConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = new SampleClass();
+            var eventInfo = sample.GetType().GetEvent("SampleEvent");
+            var methodInfo = sample.GetType().GetMethod("SampleMethod");
+            Delegate handler = Delegate.CreateDelegate(
+                eventInfo.EventHandlerType,
+                sample,
+                methodInfo);
+            eventInfo.AddEventHandler(sample, handler);
+
+            sample.RaiseEvent();
+            Console.ReadKey();
+        }
+
+        
+    }
+}
---
+++ ReflectionConsoleApplication/ReflectionConsoleApplication/SampleClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReflectionConsoleApplication
+{
+    public class SampleClass
+    {
+        public event Func<string> SampleEvent;
+
+        public string SampleMethod()
+        {
+            return "SampleMethod executed";
+        }
+
+        public void RaiseEvent()
+        {
+            if (SampleEvent != null)
+            {
+                Console.WriteLine(SampleEvent());
+            }
+        }
+    }
+}
